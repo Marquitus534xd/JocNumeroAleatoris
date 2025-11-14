32f70a8943a53fbf2b534c2d8015740d49b627f5
Marc Cepeda <marccepediosvibaldios@gmail.com>
2025-11-14 15:38:40 +0100

    Update JocNumeroAleatoris.java

--- JocNumeroAleatoris.java
+++ JocNumeroAleatoris.java
@@ -1,11 +1,11 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+ Clemnte casate con el dani y yo soy el cura y el javi pone el agua bendita
+ */ 
 
 package com.mycompany.jocnumeroaleatoris;
 import java.util.Scanner;
 /**
- *
  * @author aclemente
  */
 public class JocNumeroAleatoris {
@@ -16,13 +16,9 @@ public class JocNumeroAleatoris {
 
         int random = (int) (Math.random() * 11);
         int numero;
-        
         do{
             System.out.println("Adivina el numero aleatori: ");
             numero = llegir.nextInt();
-            
-            
-            
         }while (numero != random);
             System.out.println("Has guanyat el numero random era: " + random);
 
